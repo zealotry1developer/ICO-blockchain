a004f63a40a0e1a99a79c5612b60b346286db586
gayleblake <gyaleblakeg@gmail.com>
2023-05-09 18:53:09 -0400

    change footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,15 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="flex justify-center font-poppins font-bold m-3">
-      Made with &#10084; from &nbsp;
-      <a
-        href="https://github.com/kaamil-ahamadh"
-        target="_blank"
-        rel="noreferrer noopener"
-        className="text-cyan-500"
-      >
-        Kaamil
-      </a>
+      Copyright  2023  by  STeam
     </div>
   );
 };
